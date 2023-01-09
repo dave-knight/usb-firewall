4d1b02ed3ea665746a9cebf3c38f9e58e8bbf017
David Knight <daveknight2000@gmail.com>
2023-01-09 16:49:32 +0000

    add microsecond accuracy and timezone offset to logged timestamp

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import win32file
 import win32api
 import win32con
 import struct
+import time
+from datetime import datetime, timedelta
 
-def handle_device_add(device_path):
+def handle_device_add(device_path, device_id, product_id):
     # Open the input device
     h_input_device = win32file.CreateFile(
         device_path,
@@ -15,6 +17,16 @@ def handle_device_add(device_path):
     win32file.SetCommMask(h_input_device, win32con.EV_RXCHAR)
     win32file.SetCommState(h_input_device, win32file.DCB(ByteSize=8, BaudRate=115200, Flags=win32file.fBinary, fRtsControl=1))
     win32file.SetCommTimeouts(h_input_device, win32file.COMMTIMEOUTS(ReadIntervalTimeout=250))
+    
+    # Get timezone offset
+    now = datetime.now()
+    offset = now.utcoffset()
+
+    if offset is not None:
+        # Format the offset as a string
+       offset_str = '{:+03d}:{:02d}'.format(offset.seconds // 3600, (offset.seconds // 60) % 60)
+    else:
+        offset_str = '+00:00'
 
     # Start logging input
     with open('input.log', 'a') as log_file:
@@ -29,7 +41,8 @@ def handle_device_add(device_path):
             event_value = event_struct[2]
 
             # Format the input event as a string
-            event_str = 'type: {}, code: {}, value: {}'.format(event_type, event_code, event_value)
+            timestamp = time.strftime("%d%m%Y %H:%M:%S.%f", time.gmtime(), offset_str)
+            event_str = '{} {} {} {} {} {}'.format(timestamp, device_id, product_id, event_type, event_code, event_value)
 
             # Write the event to the log file and standard output
             log_file.write('{}\n'.format(event_str))
@@ -49,7 +62,12 @@ def start_monitoring():
 
         # Handle each new device
         for device_path in new_devices:
-            handle_device_add(device_path)
+            # Get the device's ID and product ID
+            device_id = win32api.GetVolumeInformation(device_path)[4]
+            product_id = win32api.GetVolumeInformation(device_path)[5]
+
+            # Log the input from the device
+            handle_device_add(device_path, device_id, product_id)
             logged_devices.add(device_path)
 
 if __name__ == '__main__':
